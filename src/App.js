3e562c5560b494c16a3354ff46296031c1c2fe21
James Scoville <jamesscoville@users.noreply.github.com>
2019-01-07 22:59:05 -0500

    Late night dev sesh
    * Lots of styles and animations added.
    * Updated beer list to accept some new props and to properly show details.
    * No clue how to improve my api response handling...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.scss';
 import Branding from './modules/components/Branding';
 import BeerList from './modules/components/BeerList';
-import BeerSelections from './modules/components/BeerSelections';
 
 
 export default class App extends Component {
@@ -13,10 +12,9 @@ export default class App extends Component {
       <div className="app">
         <header>
           <Branding />
-          <BeerSelections />
         </header>
         <main>
-          <BeerList order="random" randomCount="4" />
+          <BeerList order="random" randomCount="4" hasLabels="Y"/>
         </main>
       </div>
     );
--- src/modules/components/BeerList.js
+++ src/modules/components/BeerList.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+class BeerButton extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        console.log(this.props.BeerId);
+    }
+
+    render(){
+        return <button onClick={this.handleClick}>Select Beer</button>;
+    }
+}
+
 export default class BeerList extends Component {
     constructor(props) {
         super(props);
+
+        //Setup initial state
         this.state = {
             beers: [],
             isLoading: false,
@@ -12,14 +30,27 @@ export default class BeerList extends Component {
     }
 
     componentDidMount() {    
+        //Ensure loading state is updated
         this.setState({ isLoading: true });
+
+        //Based on props construct an API query string
         let apiQuery = "beers?key=78fa30f6b70c79b960afd1d38d45117c";
+        //If passed an order
         if(this.props.order){
             apiQuery += "&order=" + this.props.order;
-            if(this.props.randomCount){
+            //If random and passed a random count
+            if(this.props.order === "random" && this.props.randomCount){
                 apiQuery += "&randomCount=" + this.props.randomCount;
             }
         }
+        //If passed hasLabels
+        if(this.props.hasLabels){
+            apiQuery += "&hasLabels=" + this.props.hasLabels;
+        }
+        //Take a look at the full query string
+        //console.log(apiQuery);
+
+        //Make Api Call and setState with results
         axios.get(apiQuery)
         .then(res => {
             const beers = res.data; 
@@ -28,37 +59,59 @@ export default class BeerList extends Component {
     }
 
     render() {
+        //Pass in state
         const { beers, isLoading, error } = this.state;
         
+        //Error State
         if (error) {
             return <p>{error.message}</p>;
         }
-
+        //Loading State
         if (isLoading) {
             return <p>Loading ...</p>;
         }
 
         return (
+            //Probably a better way to handle the data away from the 
+            //component's ui itself but not worth the time.
             <div className="beer-list">
                 {beers.map((beer) =>{ 
-                    let id = beer.id;
-                    let name = beer.name; 
-                    let img = beer.labels;
-                    let information = [{key:"ABV (Alcohol by Volume)", value:beer.abv},
-                        {key:"IBU", value:beer.ibu},
-                        {key:"Description", value:beer.description},
-                        //{key:"Glass", value:beer.glass.name},
-                        {key:"Style", value:beer.style.name},
-                        //{key:"Food Pairings", value:beer.foodPairings}
-                    ];
+                    let id = beer.id,
+                        name = beer.name,
+                        img = beer.labels,
+                        information =  [{key:"ABV (Alcohol By Volume)", value:beer.abv},
+                                        {key:"IBU (International Bittering Units)", value:beer.ibu},
+                                        {key:"Glass", value:beer.glass},
+                                        {key:"Style", value:beer.style.name},
+                                        {key:"Food Pairings", value:beer.foodPairings},
+                                        {key:"Description", value:beer.description}];
+
                     return(
-                        <div className="card" key={beer.id}><h2>{name}</h2>
-                        <dl>{information.map(info => <React.Fragment><dt>{info.key}</dt><dd>{info.value}</dd></React.Fragment>)}</dl></div>
+                        <div className="card" key={id}>
+                            <div className="content">
+                                <div className="image-container">
+                                    {(img.medium != null && img.medium.length > 0) ?
+                                    (<img src={img.medium} alt={name}/>) : 
+                                    null}
+                                </div>
+                                <h2>{name}</h2>
+                                <dl>
+                                    {information.map((info, i) => 
+                                        (info.value != null && info.value.length > 0) ?
+                                        (<React.Fragment key={id + i}>
+                                            <dt>{info.key}</dt>
+                                            <dd>{info.value}</dd>
+                                        </React.Fragment>) : null
+                                    )}
+                                </dl>
+                            </div>
+                            <div className="actions">
+                                <BeerButton BeerId={id}/>
+                            </div>
+                        </div>
                     )}
                 )}
             </div>
         )
     }
 }
-//{ this.state.beers.map(beer => <li>{beer.name}</li>)}
-//<dt>ABV (Alcohol by volume)</dt><dd>{beer.abv}</dd>
